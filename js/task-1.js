49d307d00191db818a115abd3109a564a33cf4f7
Білецька <khrista.biletcka7@gmail.com>
2026-03-26 15:59:06 +0200

    main

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('.item');
+console.log(`Number of categories: ${categories.length}`);
+categories.forEach(category => {
+  const title = category.querySelector('h2').textContent;
+  const items = category.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${items}`);
+});
